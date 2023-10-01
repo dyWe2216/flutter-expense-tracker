685c8f3e4a06939119679526da43fb0c20dfc9fe
위대영 <widaeyeong@widaeyeongsMini.T-mobile.com>
2023-10-01 17:41:24 +0900

    feat: 위젯 폴더 생성 및 폴더 구조 변경

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:flutter_expense_tracker/models/expense.dart';
 
 class Expenses extends StatefulWidget {
@@ -26,11 +27,13 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          Text('The chart'),
-          Text('Expenses list...'),
+          const Text('The chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
         ],
       ),
     );
