f40d7b24ddd49357a30dc733aa70c085d16ef4ab
위대영 <widaeyeong@widaeyeongsMini.T-mobile.com>
2023-10-02 09:06:05 +0900

    feat: date picker 구현

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:flutter_expense_tracker/models/expense.dart';
+import 'package:flutter_expense_tracker/widgets/new_expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -27,7 +28,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
-        context: context, builder: (ctx) => Text('Modal bottom sheet'));
+        context: context, builder: (ctx) => const NewExpense());
   }
 
   @override
