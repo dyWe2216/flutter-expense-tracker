8c1b335732d7e3ac4e5a0eee181b465643ca98bc
위대영 <widaeyeong@widaeyeongsMini.T-mobile.com>
2023-10-02 09:13:00 +0900

    refactor: datepicker 날짜 선택 시 보여주는 텍스트 조건 추가

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_expense_tracker/models/expense.dart';
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -12,16 +13,21 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
 
-  void _presentDatePicker() {
+  void _presentDatePicker() async {
     final now = DateTime.now();
     final firstDate = DateTime(now.year - 1, now.month, now.day);
+    final pickedDate = await showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,
+    );
 
-    showDatePicker(
-        context: context,
-        initialDate: now,
-        firstDate: firstDate,
-        lastDate: now);
+    setState(() {
+      _selectedDate = pickedDate;
+    });
   }
 
   @override
@@ -64,7 +70,11 @@ class _NewExpenseState extends State<NewExpense> {
                     mainAxisAlignment: MainAxisAlignment.end,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      const Text('Selected Date'),
+                      Text(
+                        _selectedDate != null
+                            ? formatter.format(_selectedDate!)
+                            : 'No date selected',
+                      ),
                       IconButton(
                           onPressed: _presentDatePicker,
                           icon: const Icon(Icons.calendar_month))
